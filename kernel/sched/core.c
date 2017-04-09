79491c2c4463382db0538476f36fd7c1bded97d1
Rakib Mullick <rakib.mullick@gmail.com>
2017-04-09 07:36:14 +0600

    cpuset: Remove cpuset_update_active_cpus()'s parameter.
    In cpuset_update_active_cpus(), cpu_online isn't used anymore. Remove
    it.
    
    Signed-off-by: Rakib Mullick<rakib.mullick@gmail.com>
    Acked-by: Zefan Li <lizefan@huawei.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -7527,7 +7527,7 @@ void cpuset_cpu_active(void)
 		 */
 		cpuset_force_rebuild();
 	}
-	cpuset_update_active_cpus(true);
+	cpuset_update_active_cpus();
 }
 
 int cpuset_cpu_inactive(unsigned int cpu)
@@ -7550,7 +7550,7 @@ int cpuset_cpu_inactive(unsigned int cpu)
 
 		if (overflow)
 			return -EBUSY;
-		cpuset_update_active_cpus(false);
+		cpuset_update_active_cpus();
 	} else {
 		num_cpus_frozen++;
 		partition_sched_domains(1, NULL, NULL);
