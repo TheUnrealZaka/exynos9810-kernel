f52568f2a132e0ba57147a335db926dce161953f
duhansysl <duhan2002@gmail.com>
2025-06-11 14:44:28 +0300

    Revert "BACKPORT: bpf: Sysctl hook"
    This reverts commit 2c04003c9bb08739ff2963271c3a4b225466edb6.

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -1395,9 +1395,6 @@ static int bpf_prog_attach(const union bpf_attr *attr)
 	case BPF_CGROUP_SMAP_INGRESS:
 		ptype = BPF_PROG_TYPE_SK_SKB;
 		break;
-	case BPF_CGROUP_SYSCTL:
-		ptype = BPF_PROG_TYPE_CGROUP_SYSCTL;
-		break;
 	default:
 		return -EINVAL;
 	}
@@ -1466,9 +1463,6 @@ static int bpf_prog_detach(const union bpf_attr *attr)
 	case BPF_CGROUP_DEVICE:
 			ptype = BPF_PROG_TYPE_CGROUP_DEVICE;
 			break;
-	case BPF_CGROUP_SYSCTL:
-		ptype = BPF_PROG_TYPE_CGROUP_SYSCTL;
-		break;
 	default:
 		return -EINVAL;
 	}
@@ -1514,7 +1508,6 @@ static int bpf_prog_query(const union bpf_attr *attr,
 	case BPF_CGROUP_INET4_CONNECT:
 	case BPF_CGROUP_INET6_CONNECT:
 	case BPF_CGROUP_SOCK_OPS:
-	case BPF_CGROUP_SYSCTL:
 		break;
 	default:
 		return -EINVAL;
