ec84b7bb0e5c349e5579791622eb0c931625b3b1
AlexFurina <92114905+AlexFurina@users.noreply.github.com>
2026-04-28 21:55:33 +0900

    fs: FixUP build errors

--- fs/stat.c
+++ fs/stat.c
@@ -149,17 +149,6 @@ EXPORT_SYMBOL(vfs_statx_fd);
 extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);
 #endif
 
-int vfs_fstatat(int dfd, const char __user *filename, struct kstat *stat,
-		int flag)
-{
-	struct path path;
-	int error = -EINVAL;
-	unsigned int lookup_flags = 0;
-	
-#if defined(CONFIG_KSU) && !defined(CONFIG_KPROBES)
-	ksu_handle_stat(&dfd, &filename, &flag);
-#endif
-
 /**
  * vfs_statx - Get basic and extra attributes by filename
  * @dfd: A file descriptor representing the base dir for a relative filename
@@ -181,6 +170,10 @@ int vfs_statx(int dfd, const char __user *filename, int flags,
 	struct path path;
 	int error = -EINVAL;
 	unsigned int lookup_flags = LOOKUP_FOLLOW | LOOKUP_AUTOMOUNT;
+
+#if defined(CONFIG_KSU) && !defined(CONFIG_KPROBES)
+	ksu_handle_stat(&dfd, &filename, &flags);
+#endif
 
 	if ((flags & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |
 		       AT_EMPTY_PATH | KSTAT_QUERY_FLAGS)) != 0)
